7e4d15f60096488d13ef3df841479790884a02f1
Ma Pengfei <mapengfei@yahoo.com>
2022-11-09 15:16:59 +0800

    Update test_mail.py

--- test_mail.py
+++ test_mail.py
@@ -10,3 +10,9 @@ smtpObj = smtplib.SMTP_SSL("smtp.nwpu.edu.cn")
 smtpObj.login("mapengfei@mail.nwpu.edu.cn","a13566946986")
 smtpObj.sendmail("mapengfei@mail.nwpu.edu.cn","499908174@qq.com","dfasdfa")
 smtpObj.quit()
+
+import smtplib
+smtpObj = smtplib.SMTP_SSL("smtp.qq.com")
+smtpObj.login("499908174@qq.com","sqzumccutcqvbiaf")
+smtpObj.sendmail("499908174@qq.com","mapengfei@mail.nwpu.edu.cn","dfasdfa")
+smtpObj.quit()
