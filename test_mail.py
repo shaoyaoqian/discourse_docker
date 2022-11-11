c7e8e71e28cfe7c3609bf9c4d9afc9b7c1888676
Ma Pengfei <115222128+shaoyaoqian@users.noreply.github.com>
2022-11-11 10:53:13 +0800

    Update test_mail.py

--- test_mail.py
+++ test_mail.py
@@ -14,5 +14,15 @@ smtpObj.quit()
 import smtplib
 smtpObj = smtplib.SMTP_SSL("smtp.qq.com")
 smtpObj.login("499908174@qq.com","sqzumccutcqvbiaf")
-smtpObj.sendmail("499908174@qq.com","mapengfei@mail.nwpu.edu.cn","dfasdfa")
+smtpObj.sendmail("499908174@qq.com","mapengfei@mail.nwpu.edu.cn","ddddfasdfa")
 smtpObj.quit()
+
+import smtplib
+smtpObj = smtplib.SMTP_SSL("smtp.163.com",465)
+smtpObj.login("merryjingle@163.com","OCDVFOFNDTXLIXGN")
+smtpObj.sendmail("merryjingle@163.com","merryjingle@163.com","ddddfasdfa")
+smtpObj.quit()
+
+swaks --to 499908174@qq.com --from mapengfei@mail.nwpu.edu.cn --server smtp.nwpu.edu.cn --auth login --auth-user mapengfei@mail.nwpu.edu.cn  -p 25
+swaks --to mapengfei@mail.nwpu.edu.cn --from 499908174@qq.com --server smtp.qq.com --auth plain --auth-user 499908174@qq.com  -tls -p 587
+swaks --to 499908174@qq.com --from merryjingle@163.com --server smtp.163.com --auth login --auth-user merryjingle@163.com -p 25
